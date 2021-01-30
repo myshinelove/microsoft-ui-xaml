59ebf8fac7462c18841b8d37f109ffdd7f53e83a
Regis Brid <regisb@microsoft.com>
2021-01-29 17:29:17 -0800

    Experimental install & usage of Insider Windows SDK (6)

--- tools/InternalWindowsSDKNuget/MergeNupkgParts.cmd
+++ tools/InternalWindowsSDKNuget/MergeNupkgParts.cmd
@@ -8,7 +8,11 @@ call robocopy /E %~dp0..\..\packages\InternalWindowsSDK-part1.1.0.1\windowssdk-2
 echo robocopy /E %~dp0..\..\packages\InternalWindowsSDK-part2.1.0.1\windowssdk-21296.1000.210119-1600-part2 %~dp0..\..\packages\InternalWindowsSDK.1.0.1
 call robocopy /E %~dp0..\..\packages\InternalWindowsSDK-part2.1.0.1\windowssdk-21296.1000.210119-1600-part2 %~dp0..\..\packages\InternalWindowsSDK.1.0.1
 
-REM robocopy returns 0 for no files copied, 1 for files copied. (https://support.microsoft.com/en-us/kb/954404)
+REM robocopy returns:
+REM - 0x0 for no files copied,
+REM - 0x1 for files copied, (https://support.microsoft.com/en-us/kb/954404)
+REM - 0×2 for Some Extra files or directories were detected. Some housekeeping may be needed.
 IF %ERRORLEVEL%==1 set ERRORLEVEL=0
+IF %ERRORLEVEL%==3 set ERRORLEVEL=0
 
 exit /B %ERRORLEVEL%
--- tools/PublishPostBuild.cmd
+++ tools/PublishPostBuild.cmd
@@ -4,11 +4,7 @@ set ERRORLEVEL=
 
 robocopy /s %BUILD_BINARIESDIRECTORY%\signed %XES_DFSDROP%\PostBuild
 
-REM robocopy returns:
-REM - 0x0 for no files copied,
-REM - 0x1 for files copied, (https://support.microsoft.com/en-us/kb/954404)
-REM - 0×2 for Some Extra files or directories were detected. Some housekeeping may be needed.
+REM robocopy returns 0 for no files copied, 1 for files copied. (https://support.microsoft.com/en-us/kb/954404)
 IF %ERRORLEVEL%==1 set ERRORLEVEL=0
-IF %ERRORLEVEL%==3 set ERRORLEVEL=0
 
 exit /B %ERRORLEVEL%
