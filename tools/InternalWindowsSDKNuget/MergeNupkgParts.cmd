5df27bae77ceda6878d6f2340f437b4606546078
Regis Brid <regisb@microsoft.com>
2021-01-29 17:08:17 -0800

    Experimental install & usage of Insider Windows SDK (4)

--- tools/InternalWindowsSDKNuget/MergeNupkgParts.cmd
+++ tools/InternalWindowsSDKNuget/MergeNupkgParts.cmd
@@ -8,5 +8,7 @@ call robocopy /E %~dp0..\..\packages\InternalWindowsSDK-part1.1.0.1\windowssdk-2
 echo robocopy /E %~dp0..\..\packages\InternalWindowsSDK-part2.1.0.1\windowssdk-21296.1000.210119-1600-part2 %~dp0..\..\packages\InternalWindowsSDK.1.0.1
 call robocopy /E %~dp0..\..\packages\InternalWindowsSDK-part2.1.0.1\windowssdk-21296.1000.210119-1600-part2 %~dp0..\..\packages\InternalWindowsSDK.1.0.1
 
-:END
+REM robocopy returns 0 for no files copied, 1 for files copied. (https://support.microsoft.com/en-us/kb/954404)
+IF %ERRORLEVEL%==1 set ERRORLEVEL=0
+
 exit /B %ERRORLEVEL%
