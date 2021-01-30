f9e9067234288befec89dde3ba5ccf9fcf2a3205
Regis Brid <regisb@microsoft.com>
2021-01-29 17:20:26 -0800

    Experimental install & usage of Insider Windows SDK (5)

--- tools/PublishPostBuild.cmd
+++ tools/PublishPostBuild.cmd
@@ -4,7 +4,11 @@ set ERRORLEVEL=
 
 robocopy /s %BUILD_BINARIESDIRECTORY%\signed %XES_DFSDROP%\PostBuild
 
-REM robocopy returns 0 for no files copied, 1 for files copied. (https://support.microsoft.com/en-us/kb/954404)
+REM robocopy returns:
+REM - 0x0 for no files copied,
+REM - 0x1 for files copied, (https://support.microsoft.com/en-us/kb/954404)
+REM - 0×2 for Some Extra files or directories were detected. Some housekeeping may be needed.
 IF %ERRORLEVEL%==1 set ERRORLEVEL=0
+IF %ERRORLEVEL%==3 set ERRORLEVEL=0
 
 exit /B %ERRORLEVEL%
